04b25555a38975c9982efbc53b07a1b0e819a7cd
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-11 18:21:06 +0200

    feature: add schema to edit edito page

--- schemas/documents/page.js
+++ schemas/documents/page.js
@@ -2,11 +2,44 @@ export default {
   name: 'page',
   title: 'Page',
   type: 'document',
+  fieldsets: [
+    { name: 'topSection', title: 'Haut de page' },
+    { name: 'partnersSection', title: 'Partenaires' },
+    { name: 'contactSection', title: 'Contact' },
+  ],
   fields: [
     {
       name: 'pageName',
       title: 'Nom de la page',
+      description: 'NE PAS MODIFIER!',
       type: 'string',
+    },
+    // Top Section
+    {
+      name: 'topTitle',
+      title: 'Titre',
+      type: 'string',
+      fieldset: 'topSection',
+    },
+    {
+      name: 'topContent',
+      title: 'Contenu',
+      type: 'text',
+      fieldset: 'topSection',
+    },
+    // Partners Section
+    {
+      name: 'partnersTitle',
+      title: 'Titre',
+      type: 'string',
+      fieldset: 'partnersSection',
+    },
+    // Contact Section
+    {
+      name: 'contactContent',
+      title: 'Contenu',
+      type: 'text',
+      fieldset: 'contactSection',
     },
   ],
 };
