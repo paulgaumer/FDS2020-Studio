216d1f31830b348d8c7d37a0778daa6ebf6cebdb
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-12 18:09:50 +0200

    feature: add rich text block to edito topContent

--- schemas/objects/customPages/editorialPage.js
+++ schemas/objects/customPages/editorialPage.js
@@ -18,8 +18,42 @@ export default {
     {
       name: 'topContent',
       title: 'Contenu',
-      type: 'text',
       fieldset: 'topSection',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          // For titles (h1, h2..)
+          styles: [{ title: 'Normal', value: 'normal' }],
+          // For ol or ul lists
+          lists: [],
+          // Marks let you mark up inline text in the block editor.
+          marks: {
+            // Decorators usually describe a single property – e.g. a typographic
+            // preference or highlighting by editors.
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Emphasis', value: 'em' },
+            ],
+            // Annotations can be any object structure – e.g. a link or a footnote.
+            annotations: [
+              {
+                title: 'URL',
+                name: 'link',
+                type: 'object',
+                fields: [
+                  {
+                    title: 'URL',
+                    name: 'href',
+                    type: 'url',
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
     },
     // Partners Section
     {
