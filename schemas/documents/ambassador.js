917d710751bd74fed868a9da9bb8eaba360aabf5
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-12 17:25:51 +0200

    features: add features to blockContent object

--- schemas/documents/ambassador.js
+++ schemas/documents/ambassador.js
@@ -21,8 +21,34 @@ export default {
         {
           title: 'Block',
           type: 'block',
+          // For titles (h1, h2..)
           styles: [{ title: 'Normal', value: 'normal' }],
+          // For ol or ul lists
           lists: [],
+          // Marks let you mark up inline text in the block editor.
+          marks: {
+            // Decorators usually describe a single property – e.g. a typographic
+            // preference or highlighting by editors.
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Emphasis', value: 'em' },
+            ],
+            // Annotations can be any object structure – e.g. a link or a footnote.
+            annotations: [
+              {
+                title: 'URL',
+                name: 'link',
+                type: 'object',
+                fields: [
+                  {
+                    title: 'URL',
+                    name: 'href',
+                    type: 'url',
+                  },
+                ],
+              },
+            ],
+          },
         },
       ],
     },
