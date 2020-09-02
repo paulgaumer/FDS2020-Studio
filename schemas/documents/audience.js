61b2aee510936abad8246657fcf5db67617fc1be
Paul Gaumer <mitsudomoe8@gmail.com>
2020-09-02 15:38:27 +0200

    feature: add "cible scolaire" boolean to audience document

--- schemas/documents/audience.js
+++ schemas/documents/audience.js
@@ -2,12 +2,19 @@ export default {
   name: 'audience',
   title: 'Public',
   type: 'document',
+  initialValue: { audienceEducation: false },
   fields: [
     {
       name: 'name',
       title: 'Nom',
       type: 'string',
       validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'audienceEducation',
+      title: 'Cible Scolaire',
+      description: '',
+      type: 'boolean',
     },
   ],
 };
