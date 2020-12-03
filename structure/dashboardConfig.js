1ea59716ddfaf9b157ce506c4801d403a4815258
Paul Gaumer <mitsudomoe8@gmail.com>
2020-12-03 12:25:39 +0100

    feature: switch build hook to Netlify temp website

--- structure/dashboardConfig.js
+++ structure/dashboardConfig.js
@@ -9,9 +9,16 @@ export default {
         sites: [
           {
             title: 'Fête de la Science, Pays de la Loire',
-            apiId: '699d31df-6378-4d92-bc43-a0b61ea0aa4e',
-            buildHookId: '5faad0490bd16c0ed63362b2',
-            name: 'Sanity Hook',
+            // **** PRODUCTION SITE ****
+            // apiId: '699d31df-6378-4d92-bc43-a0b61ea0aa4e',
+            // buildHookId: '5faad0490bd16c0ed63362b2',
+            // name: 'Sanity Hook',
+            // *****************************
+            // **** TEMP SITE ****
+            apiId: '92497510-40a8-4eea-acf4-a937fa6920a7',
+            buildHookId: '5fc8ca7dc5319826ace64f72',
+            name: 'fds_temp_build_hook',
+            // *****************************
           },
         ],
       },
