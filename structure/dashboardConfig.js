45514b94f908cdd0153a1aa5320502d6e5161c4c
Paul Gaumer <mitsudomoe8@gmail.com>
2021-03-11 11:03:24 +0100

    Switch dahsboard build trigger to main site

--- structure/dashboardConfig.js
+++ structure/dashboardConfig.js
@@ -10,14 +10,14 @@ export default {
           {
             title: 'Fête de la Science, Pays de la Loire',
             // **** PRODUCTION SITE ****
-            // apiId: '699d31df-6378-4d92-bc43-a0b61ea0aa4e',
-            // buildHookId: '5faad0490bd16c0ed63362b2',
-            // name: 'Sanity Hook',
+            apiId: '699d31df-6378-4d92-bc43-a0b61ea0aa4e',
+            buildHookId: '5faad0490bd16c0ed63362b2',
+            name: 'Sanity Hook',
             // *****************************
             // **** TEMP SITE ****
-            apiId: '92497510-40a8-4eea-acf4-a937fa6920a7',
-            buildHookId: '5fc8ca7dc5319826ace64f72',
-            name: 'fds_temp_build_hook',
+            // apiId: '92497510-40a8-4eea-acf4-a937fa6920a7',
+            // buildHookId: '5fc8ca7dc5319826ace64f72',
+            // name: 'fds_temp_build_hook',
             // *****************************
           },
         ],
