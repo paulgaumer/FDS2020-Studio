0dbe2518c15e3c08e81dbabe03480e149f3a910a
Paul Gaumer <mitsudomoe8@gmail.com>
2021-06-26 19:32:22 +0200

    delete LimitedArray resolver
    it was creating build failure with Sanity v2

--- parts/inputResolver.js
+++ parts/inputResolver.js
@@ -1,16 +1,9 @@
-import LimitedArray from '../components/LimitedArray';
 import MapboxInputAddress from '../plugins/mapbox-input-address/src/GeopointInput';
 
 export default function resolveInput(type) {
-  const { name, options } = type;
+  const { name } = type;
 
-  if (
-    name === 'array' &&
-    options &&
-    (options.hasOwnProperty('limit') || options.hasOwnProperty('typeLimits'))
-  ) {
-    return LimitedArray;
-  } else if (name === 'addressGps') {
+  if (name === 'addressGps') {
     return MapboxInputAddress;
   }
 }
--- schemas/documents/page.js
+++ schemas/documents/page.js
@@ -23,7 +23,6 @@ export default {
       ],
       options: {
         editModal: 'fullscreen',
-        limit: 1,
       },
     },
     {
