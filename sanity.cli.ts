b29234ac3477114424f1248529250c6a365ffccb
PSalaun <pierresalaun10@hotmail.com>
2026-03-05 22:06:14 +0100

    add appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: "79da4d87fff895cda720d767",
   },
 });
